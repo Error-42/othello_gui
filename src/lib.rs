af5c579d1be16719fde0f5cb279b08c7aee52e99
Error-42 <erroruplus002d42@gmail.com>
2022-11-29 22:56:36 +0100

    implemented AIRunHandle::check()

--- src/lib.rs
+++ src/lib.rs
@@ -56,8 +56,8 @@ pub enum AIRunResult {
     Running,
     TimeOut,
     RuntimeError(ExitStatus),
-    InvalidOuput,
-    ExitedProperly(Vec2),
+    InvalidOuput(String),
+    Success(Vec2),
 }
 
 pub struct AIRunHandle {
@@ -70,7 +70,42 @@ impl AIRunHandle {
     pub fn check(&mut self) -> io::Result<AIRunResult> {
         match self.child.try_wait()? {
             Some(status) => {
-                todo!()
+                if !status.success() {
+                    Ok(AIRunResult::RuntimeError(status))
+                }
+                else {
+                    let mut output = String::new();
+
+                    self.child
+                        .stdout
+                        .as_mut()
+                        .expect("Error getting stdout of program") 
+                        .read_to_string(&mut output)
+                        .expect("Error reading stdout of program");
+
+                    let output = output.trim();
+
+                    if output.len() != 2 {
+                        return Ok(AIRunResult::InvalidOuput(format!("Output '{}' has invalid length", output)));
+                    }
+
+                    let x_char = output.chars().nth(0).unwrap();
+
+                    if x_char < 'a' || x_char > 'h' {
+                        return Ok(AIRunResult::InvalidOuput(format!("Output '{}' has invalid x coordinate", output)));
+                    }
+
+                    let y_char = output.chars().nth(1).unwrap();
+
+                    if y_char < '1' || y_char > '8' {
+                        return Ok(AIRunResult::InvalidOuput(format!("Output '{}' has invalid y coordinate", output)));
+                    }
+
+                    let x = x_char as u32 - 'a' as u32;
+                    let y = y_char as u32 - '1' as u32;
+
+                    Ok(AIRunResult::Success(Vec2::new(x as isize, y as isize)))
+                }
             }
             None => {
                 if self.start.elapsed() > self.time_limit {
