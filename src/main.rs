7da54f5a21bed5e9933fabb16e8bff56a8d7b9ae
Error-42 <erroruplus002d42@gmail.com>
2023-01-13 01:34:50 +0100

    bump version

--- src/main.rs
+++ src/main.rs
@@ -125,7 +125,7 @@ fn print_help(program_name: &str) {
 }
 
 fn print_version_info() {
-    println!("Othello GUI v0.9.0 by Error-42");
+    println!("Othello GUI v0.10.0 by Error-42");
     println!();
 }
 
