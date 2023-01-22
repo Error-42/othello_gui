9ff044e9b2d00d983e778e03bef5fae0ec4b330d
Error-42 <erroruplus002d42@gmail.com>
2023-01-22 18:17:56 +0100

    bump version

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::str::FromStr;
 use std::time::Duration;
 use std::{env, process};
 
-const VERSION: &str = "0.12.0-dev.1";
+const VERSION: &str = "0.12.0-dev.2";
 
 fn main() {
     nannou::app(model).event(event).update(update).run();
